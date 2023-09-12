b4c92ff386a89572d33656a05feeb78fcba17a8e
Harlan Ferguson <harlanjazz@hotmail.com>
2023-09-12 09:32:00 -0400

    added lombok and constructors

--- microservices-parent/product-service/src/main/java/ca/gbc/productservice/model/Product.java
+++ microservices-parent/product-service/src/main/java/ca/gbc/productservice/model/Product.java
@@ -1,8 +1,25 @@
 package ca.gbc.productservice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Document(value = "product")
 public class Product {
+
+    @Id
+    private String Id;
+    private String Name;
+    private String Description;
+    private BigDecimal price;
 
 }
