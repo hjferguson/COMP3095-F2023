6461932541ef87ca8b028890e552cfa98dbbbd85
Harlan Ferguson <harlanjazz@hotmail.com>
2023-09-18 18:46:20 -0400

    added new dto files

--- microservices-parent/product-service/src/main/java/ca/gbc/productservice/controller/ProductController.java
+++ microservices-parent/product-service/src/main/java/ca/gbc/productservice/controller/ProductController.java
@@ -1,6 +1,5 @@
 package ca.gbc.productservice.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
