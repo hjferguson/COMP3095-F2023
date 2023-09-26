bbe9195278c2a5414cbfbe4749989623c1d80418
Harlan Ferguson <harlanjazz@hotmail.com>
2023-09-26 09:04:42 -0400

    adds boilerplate tests

--- microservices-parent/product-service/src/test/java/ca/gbc/productservice/ProductServiceApplicationTests.java
+++ microservices-parent/product-service/src/test/java/ca/gbc/productservice/ProductServiceApplicationTests.java
@@ -1,11 +1,45 @@
 package ca.gbc.productservice;
 
+import ca.gbc.productservice.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    @Test
+    void createProduct(){
+
+    }
+
+    @Test
+    void getAllProducts(){
+
+    }
+
+    @Test
+    void updateProducts(){
+
+    }
+
+    @Test
+    void deleteProduct(){
+
+    }
 
 
 }
