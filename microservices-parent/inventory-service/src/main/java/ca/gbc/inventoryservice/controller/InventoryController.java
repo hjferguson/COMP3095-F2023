3f1c9478a79f495551a46c1c04c028dfe0f02e72
hjferguson <harlanjazz@gmail.com>
2023-11-06 23:06:15 -0500

    can't connect to postgres container

--- microservices-parent/inventory-service/src/main/java/ca/gbc/inventoryservice/controller/InventoryController.java
+++ microservices-parent/inventory-service/src/main/java/ca/gbc/inventoryservice/controller/InventoryController.java
@@ -14,7 +14,7 @@ public class InventoryController {
 
     @GetMapping("/{sku-code}")
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable String skuCode){
+    public boolean isInStock(@PathVariable("sku-code") String skuCode){
         return inventoryService.isInStock(skuCode);
     }
 
