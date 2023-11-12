96c9b0114ec7874d2e73bb37c68b2280d45121a3
hjferguson <harlanjazz@gmail.com>
2023-11-12 09:29:11 -0500

    refactors inventory repository

--- microservices-parent/inventory-service/src/main/java/ca/gbc/inventoryservice/repository/InventoryRepository.java
+++ microservices-parent/inventory-service/src/main/java/ca/gbc/inventoryservice/repository/InventoryRepository.java
@@ -2,12 +2,26 @@ package ca.gbc.inventoryservice.repository;
 
 import ca.gbc.inventoryservice.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 
     Optional<Inventory> findBySkuCode(String skuCode);
 
+    List<Inventory> findAllBySkuCodeIn(List<String> skuCodes);
+
+    //if you are trying to execute a unique query... you'll have to assist JPA by authoring it
+    @Query("SELECT i FROM Inventory i WHERE i.skuCode = :skuCode AND i.quantity >= :quantity")
+    List<Inventory> findBySkuCodeAndQuantity(String skuCode, Integer quantity);
+
+    default List<Inventory> findAllByInventoryRequests(List<Inventory> inventoryRequests){
+        return inventoryRequests.stream()
+                .flatMap(request -> findBySkuCodeAndQuantity(request.getSkuCode(), request.getQuantity()).stream())
+                .collect(Collectors.toList());
+    }
 
 }
